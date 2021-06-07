d12cd4a02c6c015fb59ca6428b4a17bff6243f25
Victor Huang <victorhuang1201@hotmail.com>
2021-06-07 01:59:16 -0400

    Fixed some indenting and also set up local storage for slider

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,39 +1,39 @@
 import React from 'react';
-import {withStyles} from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import DifficultySlider from './components/DifficultySlider';
 // import './App.css';
 
 const styles = {
-  root: {
-    textAlign: 'left',
-    marginLeft: 100
-  },
-  difficulty: {
-    marginTop: 50
-  },
-  appHeader: {
-    display: 'flex',
-    flexDirection: 'row',
-    fontSize: 20
-  }
+    root: {
+        textAlign: 'left',
+        marginLeft: 100
+    },
+    difficulty: {
+        marginTop: 50
+    },
+    appHeader: {
+        display: 'flex',
+        flexDirection: 'row',
+        fontSize: 20
+    }
 };
 
 function App(props) {
-	document.title = 'Dog Tricks';
-  const {classes} = props;
-  return (
-    <div className={classes.root}>
-      <div className={classes.difficulty}>
-        <h1 className={classes.appHeader}>Difficulty</h1>
-        <DifficultySlider/>
-      </div>
-    </div>
-  );
+    document.title = 'Dog Tricks';
+    const { classes } = props;
+    return (
+        <div className={classes.root}>
+            <div className={classes.difficulty}>
+                <h1 className={classes.appHeader}>Difficulty</h1>
+                <DifficultySlider />
+            </div>
+        </div>
+    );
 }
 
 App.propTypes = {
-  classes: PropTypes.object,
+    classes: PropTypes.object,
 };
 
 
--- my-app/src/components/DifficultySlider.js
+++ my-app/src/components/DifficultySlider.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {withStyles} from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import Slider from '@material-ui/core/Slider';
 
 const styles = {
@@ -15,48 +15,64 @@ const styles = {
     }
 };
 
-class DifficultySlider extends React.Component{
+class DifficultySlider extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.getSliderValue = this.getSliderValue.bind(this);
+        this.state = {
+            value: 0
+        };
+    }
+
+    componentDidMount(){
+        this.getSliderValue();
+    }
+    handleChange = (e, v) => {
+        localStorage.setItem('sliderValue', v);
+        this.setState({value: v});
+    }
+
+    getSliderValue(){
+        if (!localStorage.getItem('sliderValue')){
+            localStorage.setItem('sliderValue', 20);
+            this.setState({value: 20});
+        }
+        else{
+            this.setState({value: localStorage.getItem('sliderValue')});
+        }
+    }
 
     marks = [
-        {
-          value: 0,
-          label: 'Fundamentals',
-        },
-        {
-          value: 10,
-          label: 'Easy',
-        },
-        {
-          value: 20,
-          label: 'Medium',
-        },
-        {
-          value: 30,
-          label: 'Hard',
-        },
-      ];
+        { value: 0, label: 'Fundamentals' },
+        { value: 10, label: 'Easy' },
+        { value: 20, label: 'Medium' },
+        { value: 30, label: 'Hard' }
+    ];
 
     render() {
-        const {classes} = this.props;
-
-        return(
+        const { classes } = this.props;
+        return (
             <Slider
-            classes={{
-                root: classes.root,
-                mark: classes.mark,
-                markLabel: classes.markLabel
-            }}
-            min={0}
-            step={10}
-            max={30}
-            marks={this.marks}
+                classes={{
+                    root: classes.root,
+                    mark: classes.mark,
+                    markLabel: classes.markLabel
+                }}
+                value={this.state.value}
+                min={0}
+                step={10}
+                max={30}
+                marks={this.marks}
+                onChange={this.handleChange}
             />
-            );
+        );
     }
 }
 
 DifficultySlider.propTypes = {
     classes: PropTypes.object,
+    startValue: PropTypes.number
 };
 
 export default withStyles(styles)(DifficultySlider);
